6d759d916d41bf5fc7a50765e55c8fd9753fe174
Alessandro Borsoi <alessandroborsoi@MacBook-Pro.local>
2017-02-14 09:57:41 +0100

    Indentazione

--- input.c
+++ input.c
@@ -2,110 +2,104 @@
 
 /* buffers per la riga di input e la sua segmentazione in "tokens";
 puntatori per scorrere i buffers */
-
-static char inpbuf[MAXBUF], tokbuf[2*MAXBUF],
-	    *ptr, *tok;
+static char inpbuf[MAXBUF], tokbuf[2 * MAXBUF], *ptr, *tok;
 
 /* array di caratteri che hanno una interpretazione "speciale" 
 nei comandi */
+static char special[] = {' ', '\t', '&', ';', '\n', '\0'};
 
-static char special[]=
-	{' ', '\t', '&', ';', '\n', '\0'};
-
-int userin(char *p)		/* stampa il prompt e legge una riga */
+/* stampa il prompt e legge una riga */
+int userin(char *p)
 {
   int c, count;
-
   /* inizializzazioni per altre routines */
-
   ptr = inpbuf;
   tok = tokbuf;
-
   /* stampa il prompt */
-
-  printf("%s ",p);
-
-  count=0;
-
-  while(1) {
-
+  printf("%s ", p);
+  count = 0;
+  while (1)
+  {
     if ((c = getchar()) == EOF)
-      return(EOF);
+      return (EOF);
 
     /* si copia il carattere letto in inpbuf; ma se si raggiunge 
        e supera MAXBUF, non si scrive piu' in inpbuf, 
        si continua a leggere fino a newline (si veda sotto) */
-
     if (count < MAXBUF)
       inpbuf[count++] = c;
-
     /* se si legge il newline, la riga in input e' finita */
-
-    if (c == '\n' && count < MAXBUF) {
+    if (c == '\n' && count < MAXBUF)
+    {
       inpbuf[count] = '\0';
-      return(count);
+      return (count);
     }
-
     /*  se e' stato superato MAXBUF, quando si arriva al newline
-	si avverte che la riga e' troppo lunga e si 
+	      si avverte che la riga e' troppo lunga e si 
         va a leggere una nuova riga */
 
-    if (c == '\n') {	/* implicito se si arriva qui: count >= MAXBUF */
+    if (c == '\n')
+    { /* implicito se si arriva qui: count >= MAXBUF */
       printf("riga in input troppo lunga\n");
       count = 0;
-      printf("%s ",p);
+      printf("%s ", p);
     }
   }
 }
 
-int gettok(char **outptr)	/* legge un simbolo e lo mette in tokbuf */
+/* legge un simbolo e lo mette in tokbuf */
+int gettok(char **outptr)
 {
   int type;
 
   /* si piazza *outptr in modo che punti al primo byte dove si cominicera'
-     a scrivere il simbolo letto */  
-
+     a scrivere il simbolo letto */
   *outptr = tok;
 
   /* salta eventuali spazi */
-
-  while (*ptr == ' ' || *ptr == '\t') ptr++;
+  while (*ptr == ' ' || *ptr == '\t')
+    ptr++;
 
   /* copia il primo carattere del simbolo */
-
   *tok++ = *ptr;
 
   /* a seconda del carattere decide il tipo di simbolo */
-  
-  switch(*ptr++){
-
+  switch (*ptr++)
+  {
     case '\n':
-      type = EOL; break;
+    {
+      type = EOL;
+    }
+    break;
     case '&':
-      type = AMPERSAND; break;
+    {
+      type = AMPERSAND;
+    }
+    break;
     case ';':
-      type = SEMICOLON; break;
+    {
+      type = SEMICOLON;
+    }
+    break;
     default:
+    {
       type = ARG;
       /* copia gli altri caratteri del simbolo */
-      while(inarg(*ptr))
-	*tok++ = *ptr++;
+      while (inarg(*ptr))
+        *tok++ = *ptr++;
+    }
   }
-
   /* aggiunge \0 al fondo */
-
   *tok++ = '\0';
-  return(type);
-
+  return (type);
 }
 
-int inarg(char c)	/* verifica se c non e' un carattere speciale */
+/* verifica se c non e' un carattere speciale */
+int inarg(char c)
 {
-   char *wrk;
-
-   for (wrk = special; *wrk != '\0'; wrk++)
-       if (c == *wrk) return(0);
-
-   return(1);
+  char *wrk;
+  for (wrk = special; *wrk != '\0'; wrk++)
+    if (c == *wrk)
+      return (0);
+  return (1);
 }
-
--- smallsh.h
+++ smallsh.h
@@ -4,24 +4,23 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define EOL 1 		/* end of line */
-#define ARG 2		/* argomento normale */
-#define AMPERSAND 3 	/* & */
-#define SEMICOLON 4	/* ; */
+#define EOL 1       /* end of line */
+#define ARG 2       /* argomento normale */
+#define AMPERSAND 3 /* & */
+#define SEMICOLON 4 /* ; */
 
-#define MAXARG 512	/* numero massimo di argomenti */
-#define MAXBUF 512	/* lunghezza massima riga di input */
+#define MAXARG 512 /* numero massimo di argomenti */
+#define MAXBUF 512 /* lunghezza massima riga di input */
 
-#define	FOREGROUND 0
-#define	BACKGROUND 1
+#define FOREGROUND 0
+#define BACKGROUND 1
 
-int inarg(char c);		/* verifica se c non e' un carattere speciale */
+int inarg(char c); /* verifica se c non e' un carattere speciale */
 
-int userin(char *p);		/* stampa il prompt e legge una riga */	
+int userin(char *p); /* stampa il prompt e legge una riga */
 
-int gettok(char **outptr);	/* legge un simbolo */
+int gettok(char **outptr); /* legge un simbolo */
 
-int procline();			/* tratta una riga di input */
+int procline(); /* tratta una riga di input */
 
-void runcommand(char **cline,int where);	/* esegue un comando */
-
+void runcommand(char **cline, int where); /* esegue un comando */
